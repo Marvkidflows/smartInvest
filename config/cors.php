f2dfd28ade72284ca71a030694627391488440cd
Your Name <you@example.com>
2026-06-24 21:52:38 +0100

    fix cors allowed origin trailing slash

--- config/cors.php
+++ config/cors.php
@@ -1,10 +1,10 @@
 <?php
+
 return [
     'paths'                    => ['api/*', 'sanctum/csrf-cookie', '*'],
     'allowed_methods'          => ['*'],
     'allowed_origins'          => [
-        'https://smart-investment-three.vercel.app/',
-        'http://localhost:5173'
+        'https://smart-investment-three.vercel.app',
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers'          => ['*'],
